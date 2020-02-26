1fd973a539da1db667b589eaad6ca9556e5c2c14
AvinashBadireddy009 <badireddyavinash007@gmail.com>
2020-02-26 23:50:40 +0530

    contacts form

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,37 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => ['auth']], function () {
+    
+});
+
+Route::get('/contacts/create', [
+    'uses' => 'ContactController@create',
+    'as'   => 'contacts.create',
+]);
+
+Route::post('/contacts/store', [
+        'uses' => 'ContactController@store',
+        'as'   => 'contacts.store',
+]);
+
+Route::get('/contacts/index', [
+        'uses' => 'ContactController@index',
+        'as'   => 'contacts.index',
+]);
+
+Route::get('/contacts/{contact}/edit', [
+        'uses' => 'ContactController@edit',
+        'as'   => 'contacts.edit',
+]);
+
+Route::delete('/contacts/{contact}/delete', [
+        'uses' => 'ContactController@destroy',
+        'as'   => 'contacts.destroy',
+]);
+
+Route::patch('/contacts/{contact}/update', [
+        'uses' => 'ContactController@update',
+        'as'   => 'contacts.update',
+]);
